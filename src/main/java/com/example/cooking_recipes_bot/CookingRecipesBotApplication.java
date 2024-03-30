15c76c4dd17b043990f13da1ee6919d5e4d5349e
Parcurcik <pavelbezborodov@bk.ru>
2024-03-30 11:14:25 +0500

    upload spring boot

---
+++ src/main/java/com/example/cooking_recipes_bot/CookingRecipesBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooking_recipes_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingRecipesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingRecipesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cooking_recipes_bot/CookingRecipesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cooking_recipes_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingRecipesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
